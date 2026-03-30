16cd0386b790e301e1f4af1b6b45c728331df6ab
Amanda Cauzin <cauzinamanda@gmail.com>
2026-03-30 20:58:39 -0300

    new python dict exercises

---
+++ ex092.py
@@ -0,0 +1,6 @@
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 35}
+print(pessoas)
+print(pessoas['nome'])
+print(pessoas['idade'])
+print(pessoas['sexo'])
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos.')
